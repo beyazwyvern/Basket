02d21d45937dc79fee9a12a317cd86b552656067
kibi <onurkalkanv84@hotmail.com>
2023-10-27 21:05:58 +0300

    Update MouseLook.cs

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     public float turnSpeed = 2.0f;
+
     private float x = 0.0f;
     private float y = 0.0f;
+    bool closed = true;
+
+    private void Start()
+    {
+        transform.eulerAngles = new Vector3(0, 0, 0.0f);
+        Invoke(nameof(OpenIt), 0.5f);
+    }
+
+    void OpenIt()
+    {
+        closed = false;
+    }
 
     void Update()
     {
-        x += turnSpeed * Input.GetAxis("Mouse X");
-        y -= turnSpeed * Input.GetAxis("Mouse Y");
+        if (!closed)
+        {
+            x += turnSpeed * Input.GetAxis("Mouse X");
+            y -= turnSpeed * Input.GetAxis("Mouse Y");
 
-        transform.eulerAngles = new Vector3(y, x, 0.0f);
+            transform.eulerAngles = new Vector3(y, x, 0.0f);
+        }
     }
-
 }
