d50ce71e811c404f64a73cbc52c615512cb8047f
beyazwyvern <atillaboraaydin@hotmail.com>
2023-11-23 19:53:19 +0300

    23.11 Odev
    :)

--- Assets/Scripts/Basketball.cs
+++ Assets/Scripts/Basketball.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Basketball : MonoBehaviour
 {
+    public AudioSource source;
+    public AudioClip clip;
+
     public int speed = 1000;
     void Update()
     {
@@ -16,6 +19,7 @@ public class Basketball : MonoBehaviour
             basket.GetComponent<Rigidbody>().isKinematic = false;
             basket.GetComponent<Rigidbody>().AddForce(transform.GetChild(0).transform.forward * speed);
             Invoke(nameof(NewBall), 0.5f);
+            source.PlayOneShot(clip);
         }
     }
     void NewBall()
--- Assets/Scripts/Skor.cs
+++ Assets/Scripts/Skor.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine.SceneManagement;
+using System.Diagnostics;
 
 public class Skor : MonoBehaviour
 {
+    public AudioSource source;
+    public AudioClip clip;
+
     public int skor = 0;
     public TextMeshPro scoreText;
     public TextMeshProUGUI scoreUI;
@@ -38,18 +42,24 @@ public class Skor : MonoBehaviour
                 //kazandın
                 winFailText.text = "Kazandin!!";
                 winFailText.color = Color.green;
+
+                yield return new WaitForSeconds(2);
+                //Time.timeScale = 0;
+                yield return new WaitForSeconds(2);
+                SceneManager.LoadScene(1);
             }
             else if(skor < hedefSayi)
             {
                 //kaybettin
                 winFailText.text = "Kaybettin!!";
                 winFailText.color = Color.red;
+
+                yield return new WaitForSeconds(2);
+                endPanel.SetActive(true);
+                //Time.timeScale = 0;
+                yield return new WaitForSeconds(2);
+                SceneManager.LoadScene(0);
             }
-            yield return new WaitForSeconds(2);
-            endPanel.SetActive(true);
-            //Time.timeScale = 0;
-            yield return new WaitForSeconds(2);
-            SceneManager.LoadScene(0);
         }
     }
 
@@ -60,6 +70,7 @@ public class Skor : MonoBehaviour
             if(other.GetComponent<BasketCheck>().alt == true &&
                 other.GetComponent<BasketCheck>().ust == true)
             {
+                source.PlayOneShot(clip);
                 skor++;
                 scoreText.text = skor.ToString();
                 scoreUI.text = "Amazing!!";
